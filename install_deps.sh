7aa752cdb5760a50c8af19b5fa7d78309e3dae7e
Sebastian Kehr <51153000+stockmarketpotato@users.noreply.github.com>
2025-02-04 21:57:35 +0100

    remove commented commands

--- install_deps.sh
+++ install_deps.sh
@@ -31,29 +31,11 @@ echo 'export PATH=$PATH:$DOTNET_ROOT' >> ~/.bashrc
 export PATH=$PATH:$HOME/.dotnet/tools
 export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$PATH:$DOTNET_ROOT
-# echo "export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1" >> ~/.bashrc
 
 # --------------------------------------------------------- #
 # Install SharpFuzz
 # --------------------------------------------------------- #
 sharpfuzz/scripts/install.sh
-# #/bin/sh
-# set -eux
-
-# # Download and extract the latest afl-fuzz source package
-# wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
-# tar -xvf afl-latest.tgz
-
-# rm afl-latest.tgz
-# cd afl-2.52b/
-
-# # Install afl-fuzz
-# sudo make install
-# cd ..
-# rm -rf afl-2.52b/
-
-# # Install SharpFuzz.CommandLine global .NET tool
-# dotnet tool install --global SharpFuzz.CommandLine
 
 # --------------------------------------------------------- #
 # Build libfuzzer
